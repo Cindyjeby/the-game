3da8565ff3f05f027c88defb5422c10a8e74a0e0
cindyjeby <cindyjeby@gmail.com>
2023-12-04 15:43:30 +0300

    the game

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""start Flask"""
+
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLALchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
