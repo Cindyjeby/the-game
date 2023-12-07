5b3f71ba5d195eb5af717b2edf9a8ddd596b5f86
cindyjeby <cindyjeby@gmail.com>
2023-12-07 00:27:36 -0800

    landing page

--- app.py
+++ app.py
@@ -6,10 +6,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLALchemy(app)
+db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
